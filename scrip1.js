b0f0b9718f9f83e907bb253e656300adc71ce71a
Luis Laos <llaos.dev@gmail.com>
2024-10-24 14:51:47 -0500

    2

---
+++ scrip1.js
@@ -0,0 +1,36 @@
+pasos
+entidades :
+
+class equipo{
+    constructor (nSerie, imei,reportados,diagnostico1,pago1 ){
+
+    }
+
+}
+class cliente{
+    constructor(id, nombreCompleto, ){}
+
+}
+
+class PersonalTecnico{
+    constructor(id,dni, nombre, apellidos, sk_android, sk_ios ){
+
+    }
+
+}
+
+class diagnostico{
+    constructor(id, id_equipo, id_cliente, ){
+        
+    }
+}
+
+class repuestos{
+    constructor(id, )
+}
+
+class Reparaciones{
+    constructor(id, autorizacionEscrita,pafecha, hora, ){
+
+    }
+}
